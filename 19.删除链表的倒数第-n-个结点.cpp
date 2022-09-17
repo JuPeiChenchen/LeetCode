cae88c319b1aefd81c0af9f3bfad7bf292a3132d
JuPeichen <1911524845@qq.com>
2022-09-17 21:53:36 +0800

    first

--- 19.删除链表的倒数第-n-个结点.cpp
+++ 19.删除链表的倒数第-n-个结点.cpp
@@ -15,7 +15,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         int index = 0;
@@ -38,14 +38,29 @@ public:
             pre = pre->next;
         }
        // cout<<cur->val<<endl;
-        /*if(cur->next != NULL && cur->next->next != NULL) {
-            cur->next = cur->next->next;
-        }
-        else {
-            cur->next = NULL;
-        }*/
+        
         pre->next = cur->next;
         //delete cur;
+        head = newHead->next;
+        delete newHead;
+        return head;
+    }
+};*/
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* newHead = new ListNode(0);
+        newHead->next = head;
+        ListNode* fast = newHead;
+        ListNode* slow = newHead;
+        while(n-- && fast->next != NULL) {
+            fast = fast->next;
+        }
+        while(fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
         head = newHead->next;
         delete newHead;
         return head;
