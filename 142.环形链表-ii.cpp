f4d5d77ae53cc45e0cb58615074c31f0d12e9cea
JuPeichen <1911524845@qq.com>
2022-09-18 10:42:49 +0800

    first

---
+++ 142.环形链表-ii.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) {
+            return NULL;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) {
+                ListNode* a = fast;
+                ListNode* b = head;
+                while(a != b) {
+                     a = a->next;
+                     b = b->next;
+                }
+              
+                return b;
+            }
+            
+        }
+        return NULL;
+    }
+};
+// @lc code=end
+
