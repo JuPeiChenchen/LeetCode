c98070672728baf41a18fe881af9865bcd995de2
JuPeichen <1911524845@qq.com>
2022-09-20 18:27:35 +0800

    first

---
+++ 698.划分为k个相等的子集.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=698 lang=cpp
+ *
+ * [698] 划分为k个相等的子集
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+
+    }
+};
+// @lc code=end
+
