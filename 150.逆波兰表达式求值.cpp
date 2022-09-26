973698ce448414249849746ade8d14d90c5b0bf4
JuPeichen <1911524845@qq.com>
2022-09-26 12:57:08 +0800

    first

---
+++ 150.逆波兰表达式求值.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=150 lang=cpp
+ *
+ * [150] 逆波兰表达式求值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int num1 = st.top();
+                st.pop();
+                int num2 = st.top();
+                st.pop();
+                if (tokens[i] == "+") st.push(num2 + num1);
+                if (tokens[i] == "-") st.push(num2 - num1);
+                if (tokens[i] == "*") st.push(num2 * num1);
+                if (tokens[i] == "/") st.push(num2 / num1);
+            } else {
+                st.push(stoi(tokens[i]));
+            }
+        }
+        int result = st.top();
+        st.pop(); // 把栈里最后一个元素弹出（其实不弹出也没事）
+        return result;
+    }
+};
+// @lc code=end
+
