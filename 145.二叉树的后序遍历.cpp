e25adf916c53c2930c43833760300fab04bdc19a
JuPeichen <1911524845@qq.com>
2022-09-28 10:26:24 +0800

    first

---
+++ 145.二叉树的后序遍历.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=cpp
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* cur, vector<int>& vec) {
+        if (cur == NULL) return;
+        traversal(cur->left, vec);  // 左
+        traversal(cur->right, vec); // 右
+        vec.push_back(cur->val);    // 中
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
+// @lc code=end
+
