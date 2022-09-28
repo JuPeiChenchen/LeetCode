aa4a7dc06ef1148576fd9552ce3fa2cf527c0025
JuPeichen <1911524845@qq.com>
2022-09-28 11:04:14 +0800

    first

---
+++ 102.二叉树的层序遍历.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=cpp
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> que;
+        if(root != NULL) que.push(root);
+        vector<vector<int>> result;
+        while(!que.empty()) {
+            int size = que.size();
+            vector<int> vec;
+            for(int i = 0; i < size; i++ ) {
+                TreeNode* a = que.front();
+                que.pop();
+                if(a->left) que.push(a->left);
+                if(a->right) que.push(a->right);
+                vec.push_back(a->val);
+            }
+            result.push_back(vec);
+        }
+        return result;
+    }
+};*/
+//递归法
+class Solution {
+public:
+    void order(TreeNode* cur, vector<vector<int>>& result, int depth)
+    {
+        if (cur == nullptr) return;
+        if (result.size() == depth) result.push_back(vector<int>());
+        result[depth].push_back(cur->val);
+        order(cur->left, result, depth + 1);
+        order(cur->right, result, depth + 1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        int depth = 0;
+        order(root, result, depth);
+        return result;
+    }
+};
+// @lc code=end
+
