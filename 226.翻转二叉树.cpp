ced6e83f4779d0fa393ca9697c7ffd39d41f8a07
JuPeichen <1911524845@qq.com>
2022-09-30 09:05:21 +0800

    first

---
+++ 226.翻转二叉树.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> que;
+        if(root != NULL) que.push(root);
+        while(!que.empty()) {
+            TreeNode* q = que.front();
+            que.pop();
+            TreeNode* a = q->left;
+            q->left = q->right;
+            q->right = a;
+            if(q->left) que.push(q->left);
+            if(q->right) que.push(q->right);
+        }
+        return root;
+    }
+};*/
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        swap(root->left, root->right);  // 中
+        invertTree(root->left);         // 左
+        invertTree(root->right);        // 右
+        return root;
+    }
+};
+/*
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()) {
+            TreeNode* node = st.top();              // 中
+            st.pop();
+            swap(node->left, node->right);
+            if(node->right) st.push(node->right);   // 右
+            if(node->left) st.push(node->left);     // 左
+        }
+        return root;
+    }
+};*/
+
+// @lc code=end
+
