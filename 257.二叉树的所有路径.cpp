756ce163e752f999d5c6670f7a665d3e8b9002bd
JuPeichen <1911524845@qq.com>
2022-10-01 10:41:33 +0800

    first

---
+++ 257.二叉树的所有路径.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=cpp
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+
+    void traversal(TreeNode* cur, vector<int>& path, vector<string>& result) {
+        path.push_back(cur->val); // 中，中为什么写在这里，因为最后一个节点也要加入到path中 
+        // 这才到了叶子节点
+        if (cur->left == NULL && cur->right == NULL) {
+            string sPath;
+            for (int i = 0; i < path.size() - 1; i++) {
+                sPath += to_string(path[i]);
+                sPath += "->";
+            }
+            sPath += to_string(path[path.size() - 1]);
+            result.push_back(sPath);
+            return;
+        }
+        if (cur->left) { // 左 
+            traversal(cur->left, path, result);
+            path.pop_back(); // 回溯
+        }
+        if (cur->right) { // 右
+            traversal(cur->right, path, result);
+            path.pop_back(); // 回溯
+        }
+    }
+
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        vector<int> path;
+        if (root == NULL) return result;
+        traversal(root, path, result);
+        return result;
+    }
+};
+/*class Solution {
+private:
+    void traversal(TreeNode* cur, string path, vector<string>& result) {
+        path += to_string(cur->val); // 中，中为什么写在这里，因为最后一个节点也要加入到path中
+        if (cur->left == NULL && cur->right == NULL) {
+            result.push_back(path);
+            return;
+        }
+        if (cur->left) {
+            path += "->";
+            traversal(cur->left, path, result); // 左
+            path.pop_back(); // 回溯 '>'
+            path.pop_back(); // 回溯 '-'
+        }
+        if (cur->right) {
+            path += "->";
+            traversal(cur->right, path, result); // 右
+            path.pop_back(); // 回溯'>'
+            path.pop_back(); // 回溯 '-'
+        }
+    }
+
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        string path;
+        if (root == NULL) return result;
+        traversal(root, path, result);
+        return result;
+
+    }
+};
+*/
+/*class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        stack<TreeNode*> treeSt;// 保存树的遍历节点
+        stack<string> pathSt;   // 保存遍历路径的节点
+        vector<string> result;  // 保存最终路径集合
+        if (root == NULL) return result;
+        treeSt.push(root);
+        pathSt.push(to_string(root->val));
+        while (!treeSt.empty()) {
+            TreeNode* node = treeSt.top(); treeSt.pop(); // 取出节点 中
+            string path = pathSt.top();pathSt.pop();    // 取出该节点对应的路径
+            if (node->left == NULL && node->right == NULL) { // 遇到叶子节点
+                result.push_back(path);
+            }
+            if (node->right) { // 右
+                treeSt.push(node->right);
+                pathSt.push(path + "->" + to_string(node->right->val));
+            }
+            if (node->left) { // 左
+                treeSt.push(node->left);
+                pathSt.push(path + "->" + to_string(node->left->val));
+            }
+        }
+        return result;
+    }
+};*/
+// @lc code=end
+
