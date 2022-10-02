335df8089bd9e5808075ca50a9820af296ecdcb0
JuPeichen <1911524845@qq.com>
2022-10-02 11:28:20 +0800

    first

--- 112.路径总和.cpp
+++ 112.路径总和.cpp
@@ -71,5 +71,32 @@ public:
         return traversal(root, sum - root->val);
     }
 };*/
+/*class solution {
+
+public:
+    bool haspathsum(TreeNode* root, int sum) {
+        if (root == null) return false;
+        // 此时栈里要放的是pair<节点指针，路径数值>
+        stack<pair<TreeNode*, int>> st;
+        st.push(pair<TreeNode*, int>(root, root->val));
+        while (!st.empty()) {
+            pair<TreeNode*, int> node = st.top();
+            st.pop();
+            // 如果该节点是叶子节点了，同时该节点的路径数值等于sum，那么就返回true
+            if (!node.first->left && !node.first->right && sum == node.second) return true;
+
+            // 右节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+            if (node.first->right) {
+                st.push(pair<TreeNode*, int>(node.first->right, node.second + node.first->right->val));
+            }
+
+            // 左节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+            if (node.first->left) {
+                st.push(pair<TreeNode*, int>(node.first->left, node.second + node.first->left->val));
+            }
+        }
+        return false;
+    }
+};*/
 // @lc code=end
 
