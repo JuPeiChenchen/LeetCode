b479e82afb7b2bf6f743dda065578dff7663e439
JuPeichen <1911524845@qq.com>
2022-10-03 11:00:22 +0800

    first

---
+++ 617.合并二叉树.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=cpp
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* traversal(TreeNode* root1, TreeNode*root2) {
+        TreeNode* root = new TreeNode();
+        if(root1 == NULL && root2 == NULL) return NULL;
+        else if(root1 == NULL) {
+            root->val = root2->val;
+        }
+        else if(root2 == NULL) {
+            root->val = root1.val;
+        }
+        else {
+            root->val = root1->val + root2.val;
+        }
+        root->left = traversal(root1->left, root2->left);
+        root->right = traversal(root2->left, root2->right);
+        return root;
+    }
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1 == NULL) {
+            return root2;
+        }
+        else if(root2 == NULL) {
+            return root1;
+        }
+        TreeNode* root = new TreeNode;
+        return traversal(root1, root2);
+    }
+};
+/*class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL) return t2;
+        if (t2 == NULL) return t1;
+        queue<TreeNode*> que;
+        que.push(t1);
+        que.push(t2);
+        while(!que.empty()) {
+            TreeNode* node1 = que.front(); que.pop();
+            TreeNode* node2 = que.front(); que.pop();
+            // 此时两个节点一定不为空，val相加
+            node1->val += node2->val;
+
+            // 如果两棵树左节点都不为空，加入队列
+            if (node1->left != NULL && node2->left != NULL) {
+                que.push(node1->left);
+                que.push(node2->left);
+            }
+            // 如果两棵树右节点都不为空，加入队列
+            if (node1->right != NULL && node2->right != NULL) {
+                que.push(node1->right);
+                que.push(node2->right);
+            }
+
+            // 当t1的左节点 为空 t2左节点不为空，就赋值过去
+            if (node1->left == NULL && node2->left != NULL) {
+                node1->left = node2->left;
+            }
+            // 当t1的右节点 为空 t2右节点不为空，就赋值过去
+            if (node1->right == NULL && node2->right != NULL) {
+                node1->right = node2->right;
+            }
+        }
+        return t1;
+    }
+};*/
+// @lc code=end
+
