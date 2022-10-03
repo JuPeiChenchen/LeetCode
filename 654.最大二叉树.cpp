3eb09b1c49e83a068133ad690be53b42b14544b4
JuPeichen <1911524845@qq.com>
2022-10-03 10:25:02 +0800

    first

---
+++ 654.最大二叉树.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=cpp
+ *
+ * [654] 最大二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    // 在左闭右开区间[left, right)，构造二叉树
+    TreeNode* traversal(vector<int>& nums, int left, int right) {
+        if (left >= right) return nullptr;
+
+        // 分割点下标：maxValueIndex
+        int maxValueIndex = left;
+        for (int i = left + 1; i < right; ++i) {
+            if (nums[i] > nums[maxValueIndex]) maxValueIndex = i;
+        }
+
+        TreeNode* root = new TreeNode(nums[maxValueIndex]);
+
+        // 左闭右开：[left, maxValueIndex)
+        root->left = traversal(nums, left, maxValueIndex);
+
+        // 左闭右开：[maxValueIndex + 1, right)
+        root->right = traversal(nums, maxValueIndex + 1, right);
+
+        return root;
+    }
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return traversal(nums, 0, nums.size());
+    }
+};
+// @lc code=end
+
