1c3b819abd11f2754dd49be76aff8f5db339d3e6
JuPeichen <1911524845@qq.com>
2022-10-04 10:50:50 +0800

    first

---
+++ 236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == q || root == p || root == NULL) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left != NULL && right != NULL) return root;
+
+        if (left == NULL && right != NULL) return right;
+        else if (left != NULL && right == NULL) return left;
+        else  { //  (left == NULL && right == NULL)
+            return NULL;
+        }
+
+    }
+};
+// @lc code=end
+
