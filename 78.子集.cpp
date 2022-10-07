48b60a4d597a58b99e2472aaaa26f03037661ef7
JuPeichen <1911524845@qq.com>
2022-10-07 12:52:22 +0800

    first

---
+++ 78.子集.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex) {
+        result.push_back(path); // 收集子集，要放在终止添加的上面，否则会漏掉自己
+        if (startIndex >= nums.size()) { // 终止条件可以不加
+            return;
+        }
+        for (int i = startIndex; i < nums.size(); i++) {
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        backtracking(nums, 0);
+        return result;
+    }
+};
+// @lc code=end
+
