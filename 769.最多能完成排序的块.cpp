a04bcc547a876430e9ab50228804ffeaf4933a8f
JuPeichen <1911524845@qq.com>
2022-10-13 11:13:28 +0800

    first

--- 769.最多能完成排序的块.cpp
+++ 769.最多能完成排序的块.cpp
@@ -55,5 +55,19 @@ public:
         return result;
     }
 };
+/*class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int m = 0, res = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            m = max(m, arr[i]);
+            if (m == i) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+*/
 // @lc code=end
 
