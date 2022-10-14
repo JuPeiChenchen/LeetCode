479e175194fe0be3bb443bfdcdcc6dd1e3d99d62
JuPeichen <1911524845@qq.com>
2022-10-14 12:07:01 +0800

    first

---
+++ 940.不同的子序列-ii.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=940 lang=cpp
+ *
+ * [940] 不同的子序列 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        vector<int> last(26, -1);
+        
+        int n = s.size();
+        vector<int> f(n, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (last[j] != -1) {
+                    f[i] = (f[i] + f[last[j]]) % mod;
+                }
+            }
+            last[s[i] - 'a'] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (last[i] != -1) {
+                ans = (ans + f[last[i]]) % mod;
+            }
+        }
+        return ans;
+    }
+
+private:
+    static constexpr int mod = 1000000007;
+};
+l
+// @lc code=end
+
