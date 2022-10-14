b4618d0838ff87bf573598913e99bcab9c58acb2
JuPeichen <1911524845@qq.com>
2022-10-14 12:07:10 +0800

    first

---
+++ 509.斐波那契数.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=cpp
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int fib(int N) {
+        if (N <= 1) return N;
+        vector<int> dp(N + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[N];
+    }
+};
+/*class Solution {
+public:
+    int fib(int N) {
+        if (N < 2) return N;
+        return fib(N - 1) + fib(N - 2);
+    }
+};*/
+// @lc code=end
+
