60feacc263a42625b050f6a4f9060414ad10e7c2
JuPeichen <1911524845@qq.com>
2022-10-15 10:33:42 +0800

    first

---
+++ 746.使用最小花费爬楼梯.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=cpp
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size());
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.size(); i++) {
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        // 注意最后一步可以理解为不用花费，所以取倒数第一步，第二步的最少值
+        return min(dp[cost.size() - 1], dp[cost.size() - 2]);
+    }
+};
+// @lc code=end
+
