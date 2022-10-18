89bb81f62c49e8645e29858f55cfcf87960e9a40
JuPeichen <1911524845@qq.com>
2022-10-18 10:55:31 +0800

    first

---
+++ 902.最大为-n-的数字组合.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=902 lang=cpp
+ *
+ * [902] 最大为 N 的数字组合
+ */
+
+// @lc code=start
+/*class Solution {
+public:
+    int result;
+    string path;
+    int sum = 0;
+    void traversal(vector<string>& digits, int n) {
+        if(sum > n) {
+            return;
+            }
+        if(sum > 0) result++;
+        for(int i = 0; i < digits.size(); i++) {
+            sum  = sum * 10 + stoi(digits[i]);
+            if(sum > n) {
+            break;
+            }
+            
+            cout<<sum<<endl;
+            traversal(digits, n);
+            sum = sum / 10;
+        }
+    }
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        traversal(digits, n);
+        return result;
+    }
+};*/
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string s = to_string(n);
+        int m = digits.size(), k = s.size();
+        vector<vector<int>> dp(k + 1, vector<int>(2));
+        dp[0][1] = 1;
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < m; j++) {
+                if (digits[j][0] == s[i - 1]) {
+                    dp[i][1] = dp[i - 1][1];
+                } else if (digits[j][0] < s[i - 1]) {
+                    dp[i][0] += dp[i - 1][1];
+                } else {
+                    break;
+                }
+            }
+            if (i > 1) {
+                dp[i][0] += m + dp[i - 1][0] * m;
+            }
+        }
+        return dp[k][0] + dp[k][1];
+    }
+};
+/*class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string s = to_string(n);
+        int m = digits.size(), k = s.size();
+        vector<int> bits;
+        bool isLimit = true;
+        for (int i = 0; i < k; i++) {
+            if (!isLimit) {
+                bits.emplace_back(m - 1);
+            } else {
+                int selectIndex = -1;
+                for (int j = 0; j < m; j++) {
+                    if (digits[j][0] <= s[i]) {
+                        selectIndex = j;
+                    } else {
+                        break;
+                    }
+                }
+                if (selectIndex >= 0) {
+                    bits.emplace_back(selectIndex);
+                    if (digits[selectIndex][0] < s[i]) {
+                        isLimit = false;
+                    }
+                } else {
+                    int len = bits.size();
+                    while (!bits.empty() && bits.back() == 0) {
+                        bits.pop_back();
+                    }
+                    if (!bits.empty()) {
+                        bits.back() -= 1;
+                    } else {
+                        len--;
+                    }
+                    while ((int)bits.size() <= len) {
+                        bits.emplace_back(m - 1);
+                    }
+                    isLimit = false;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < bits.size(); i++) {
+            ans = ans * m + (bits[i] + 1);
+        }
+        return ans;
+    }
+};
+*/
+// @lc code=end
+
