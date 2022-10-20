ebcf8ca3fb3a0872226a41186661b2f6f37fc64b
JuPeichen <1911524845@qq.com>
2022-10-20 10:53:36 +0800

    first

---
+++ 494.目标和.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ *
+ * [494] 目标和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) sum += nums[i];
+        if (abs(S) > sum) return 0; // 此时没有方案
+        if ((S + sum) % 2 == 1) return 0; // 此时没有方案
+        int bagSize = (S + sum) / 2;
+	if (bagSize < 0) return 0;
+        vector<int> dp(bagSize + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
+    }
+};
+
+
+// @lc code=end
+
