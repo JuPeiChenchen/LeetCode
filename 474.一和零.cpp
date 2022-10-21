40107ffc01dc6aec281fbbf6b93cd0868352b63f
JuPeichen <1911524845@qq.com>
2022-10-21 10:44:21 +0800

    first

--- 474.一和零.cpp
+++ 474.一和零.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0)); // 默认初始化0
+        for (string str : strs) { // 遍历物品
+            int oneNum = 0, zeroNum = 0;
+            for (char c : str) {
+                if (c == '0') zeroNum++;
+                else oneNum++;
+            }
+            for (int i = m; i >= zeroNum; i--) { // 遍历背包容量且从后向前遍历！
+                for (int j = n; j >= oneNum; j--) {
+                    dp[i][j] = max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
 // @lc code=end
