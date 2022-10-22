1088f1f6b1b75fd10dbd6537c5b4cdebd9a577e5
JuPeichen <1911524845@qq.com>
2022-10-22 21:36:06 +0800

    first

---
+++ 518.零钱兑换-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < coins.size(); i++) { // 遍历物品
+            for (int j = coins[i]; j <= amount; j++) { // 遍历背包
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
+// @lc code=end
+
