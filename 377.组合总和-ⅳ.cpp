0b052cf873a8bf7146fab882c25b9989cd17d0a8
JuPeichen <1911524845@qq.com>
2022-10-23 09:38:03 +0800

    first

---
+++ 377.组合总和-ⅳ.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=cpp
+ *
+ * [377] 组合总和 Ⅳ
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) { // 遍历背包
+            for (int j = 0; j < nums.size(); j++) { // 遍历物品
+                if (i - nums[j] >= 0 && dp[i] < INT_MAX - dp[i - nums[j]]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+// @lc code=end
+
