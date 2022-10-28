2e6ee0709a772da08f27ac62b8cca088b0aebddc
JuPeichen <1911524845@qq.com>
2022-10-28 13:44:04 +0800

    first

--- 907.子数组的最小值之和.cpp
+++ 907.子数组的最小值之和.cpp
@@ -60,6 +60,27 @@ public:
 };
 */
 /*class Solution {
+    const int MOD = 1e9 + 7;
+public:
+    int sumSubarrayMins(vector<int> &arr) {
+        long ans = 0L;
+        arr.push_back(-1);
+        stack<int> st;
+        st.push(-1); // 哨兵
+        for (int r = 0; r < arr.size(); ++r) {
+            while (st.size() > 1 && arr[st.top()] >= arr[r]) {
+                int i = st.top();
+                st.pop();
+                ans += (long) arr[i] * (i - st.top()) * (r - i); // 累加贡献
+            }
+            st.push(r);
+        }
+        return ans % MOD;
+    }
+};
+
+*/
+/*class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
