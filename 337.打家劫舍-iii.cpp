f8d239b0de1795f11f7762353751acf5a286d3fb
JuPeichen <1911524845@qq.com>
2022-10-29 11:12:30 +0800

    first

---
+++ 337.打家劫舍-iii.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=337 lang=cpp
+ *
+ * [337] 打家劫舍 III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*class Solution {
+public:
+    int rob(TreeNode* root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL && root->right == NULL) return root->val;
+        // 偷父节点
+        int val1 = root->val;
+        if (root->left) val1 += rob(root->left->left) + rob(root->left->right); // 跳过root->left，相当于不考虑左孩子了
+        if (root->right) val1 += rob(root->right->left) + rob(root->right->right); // 跳过root->right，相当于不考虑右孩子了
+        // 不偷父节点
+        int val2 = rob(root->left) + rob(root->right); // 考虑root的左右孩子
+        return max(val1, val2);
+    }
+};*/
+class Solution {
+public:
+    unordered_map<TreeNode* , int> umap; // 记录计算过的结果
+    int rob(TreeNode* root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL && root->right == NULL) return root->val;
+        if (umap[root]) return umap[root]; // 如果umap里已经有记录则直接返回
+        // 偷父节点
+        int val1 = root->val;
+        if (root->left) val1 += rob(root->left->left) + rob(root->left->right); // 跳过root->left
+        if (root->right) val1 += rob(root->right->left) + rob(root->right->right); // 跳过root->right
+        // 不偷父节点
+        int val2 = rob(root->left) + rob(root->right); // 考虑root的左右孩子
+        umap[root] = max(val1, val2); // umap记录一下结果
+        return max(val1, val2);
+    }
+};
+/*class Solution {
+public:
+    int rob(TreeNode* root) {
+        vector<int> result = robTree(root);
+        return max(result[0], result[1]);
+    }
+    // 长度为2的数组，0：不偷，1：偷
+    vector<int> robTree(TreeNode* cur) {
+        if (cur == NULL) return vector<int>{0, 0};
+        vector<int> left = robTree(cur->left);
+        vector<int> right = robTree(cur->right);
+        // 偷cur，那么就不能偷左右节点。
+        int val1 = cur->val + left[0] + right[0];
+        // 不偷cur，那么可以偷也可以不偷左右节点，则取较大的情况
+        int val2 = max(left[0], left[1]) + max(right[0], right[1]);
+        return {val2, val1};
+    }
+};*/
+// @lc code=end
+
