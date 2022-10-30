76491a3b69be318cec25d15daff9d3aacf10077d
JuPeichen <1911524845@qq.com>
2022-10-30 09:52:14 +0800

    first

--- 784.字母大小写全排列.cpp
+++ 784.字母大小写全排列.cpp
@@ -59,5 +59,30 @@ public:
     }
 };
 */
+/*class Solution {
+public:
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        queue<string> qu;
+        qu.emplace("");
+        while (!qu.empty()) {
+            string &curr = qu.front();
+            if (curr.size() == s.size()) {
+                ans.emplace_back(curr);
+                qu.pop();
+            } else {
+                int pos = curr.size();
+                if (isalpha(s[pos])) {
+                    string next = curr;
+                    next.push_back(s[pos] ^ 32);
+                    qu.emplace(next);
+                }
+                curr.push_back(s[pos]);                
+            }
+        }
+        return ans;
+    }
+};
+*/
 // @lc code=end
 
