0506c7c7e275ef8671e5ce20d47844ad65a7b4ff
JuPeichen <1911524845@qq.com>
2022-11-03 09:15:16 +0800

    first

---
+++ 1668.最大重复子字符串.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=1668 lang=cpp
+ *
+ * [1668] 最大重复子字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int n = sequence.size(), m = word.size();
+        if (n < m) {
+            return 0;
+        }
+
+        vector<int> fail(m, -1);
+        for (int i = 1; i < m; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && word[j + 1] != word[i]) {
+                j = fail[j];
+            }
+            if (word[j + 1] == word[i]) {
+                fail[i] = j + 1;
+            }
+        }
+
+        vector<int> f(n);
+        int j = -1;
+        for (int i = 0; i < n; ++i) {
+            while (j != -1 && word[j + 1] != sequence[i]) {
+                j = fail[j];
+            }
+            if (word[j + 1] == sequence[i]) {
+                ++j;
+                if (j == m - 1) {
+                    f[i] = (i >= m ? f[i - m] : 0) + 1;
+                    j = fail[j];
+                }
+            }
+        }
+
+        return *max_element(f.begin(), f.end());
+    }
+};
+/*class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int n = sequence.size(), m = word.size();
+        if (n < m) {
+            return 0;
+        }
+
+        vector<int> f(n);
+        for (int i = m - 1; i < n; ++i) {
+            bool valid = true;
+            for (int j = 0; j < m; ++j) {
+                if (sequence[i - m + j + 1] != word[j]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
+            }
+        }
+        
+        return *max_element(f.begin(), f.end());
+    }
+};
+*/
+// @lc code=end
+
