5badb60ab28f908e6557cca64322865e25863113
JuPeichen <1911524845@qq.com>
2022-11-05 10:53:16 +0800

    first

---
+++ 1106.解析布尔表达式.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=1106 lang=cpp
+ *
+ * [1106] 解析布尔表达式
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> stk;
+        int n = expression.size();
+        for (int i = 0; i < n; i++) {
+            char c = expression[i];
+            if (c == ',') {
+                continue;
+            } else if (c != ')') {
+                stk.push(c);
+            } else {
+                int t = 0, f = 0;
+                while (stk.top() != '(') {
+                    char val = stk.top();
+                    stk.pop();
+                    if (val == 't') {
+                        t++;
+                    } else {
+                        f++;
+                    }
+                }
+                stk.pop();
+                char op = stk.top();
+                stk.pop();
+                switch (op) {
+                case '!':
+                    stk.push(f == 1 ? 't' : 'f');
+                    break;
+                case '&':
+                    stk.push(f == 0 ? 't' : 'f');
+                    break;
+                case '|':
+                    stk.push(t > 0 ? 't' : 'f');
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        return stk.top() == 't';
+    }
+};
+
+
+// @lc code=end
+
