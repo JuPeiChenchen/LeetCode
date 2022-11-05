bb39e77a72c4def976dcdfe5146a8e5fd1867f9d
JuPeichen <1911524845@qq.com>
2022-11-05 11:26:51 +0800

    first

---
+++ 718.最长重复子数组.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=718 lang=cpp
+ *
+ * [718] 最长重复子数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        vector<vector<int>> dp (A.size() + 1, vector<int>(B.size() + 1, 0));
+        int result = 0;
+        for (int i = 1; i <= A.size(); i++) {
+            for (int j = 1; j <= B.size(); j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                if (dp[i][j] > result) result = dp[i][j];
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
+
