42f0f2bbf2c579ca8770f1510d859c8434dc0f59
JuPeichen <1911524845@qq.com>
2022-11-27 10:53:36 +0800

    first

---
+++ 503.下一个更大元素-ii.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=503 lang=cpp
+ *
+ * [503] 下一个更大元素 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // 拼接一个新的nums
+        vector<int> nums1(nums.begin(), nums.end());
+        nums.insert(nums.end(), nums1.begin(), nums1.end());
+        // 用新的nums大小来初始化result
+        vector<int> result(nums.size(), -1);
+        if (nums.size() == 0) return result;
+
+        // 开始单调栈
+        stack<int> st;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!st.empty() && nums[i] > nums[st.top()]) {
+                result[st.top()] = nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        // 最后再把结果集即result数组resize到原数组大小
+        result.resize(nums.size() / 2);
+        return result;
+    }
+};
+/*class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> result(nums.size(), -1);
+        if (nums.size() == 0) return result;
+        stack<int> st;
+        for (int i = 0; i < nums.size() * 2; i++) {
+            // 模拟遍历两边nums，注意一下都是用i % nums.size()来操作
+            while (!st.empty() && nums[i % nums.size()] > nums[st.top()]) {
+                result[st.top()] = nums[i % nums.size()];
+                st.pop();
+            }
+            st.push(i % nums.size());
+        }
+        return result;
+    }
+};*/
+// @lc code=end
+
