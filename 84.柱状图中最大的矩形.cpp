c39165c569ac18dc47a7fb2585c0e769bdff2b8c
JuPeichen <1911524845@qq.com>
2022-11-29 20:51:49 +0800

    first

---
+++ 84.柱状图中最大的矩形.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=cpp
+ *
+ * [84] 柱状图中最大的矩形
+ */
+
+// @lc code=start
+/*class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> minLeftIndex(heights.size());
+        vector<int> minRightIndex(heights.size());
+        int size = heights.size();
+
+        // 记录每个柱子 左边第一个小于该柱子的下标
+        minLeftIndex[0] = -1; // 注意这里初始化，防止下面while死循环
+        for (int i = 1; i < size; i++) {
+            int t = i - 1;
+            // 这里不是用if，而是不断向左寻找的过程
+            while (t >= 0 && heights[t] >= heights[i]) t = minLeftIndex[t];
+            minLeftIndex[i] = t;
+        }
+        // 记录每个柱子 右边第一个小于该柱子的下标
+        minRightIndex[size - 1] = size; // 注意这里初始化，防止下面while死循环
+        for (int i = size - 2; i >= 0; i--) {
+            int t = i + 1;
+            // 这里不是用if，而是不断向右寻找的过程
+            while (t < size && heights[t] >= heights[i]) t = minRightIndex[t];
+            minRightIndex[i] = t;
+        }
+        // 求和
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            int sum = heights[i] * (minRightIndex[i] - minLeftIndex[i] - 1);
+            result = max(sum, result);
+        }
+        return result;
+    }
+};*/
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        heights.insert(heights.begin(), 0); // 数组头部加入元素0
+        heights.push_back(0); // 数组尾部加入元素0
+        st.push(0);
+        int result = 0;
+        // 第一个元素已经入栈，从下标1开始
+        for (int i = 1; i < heights.size(); i++) {
+            // 注意heights[i] 是和heights[st.top()] 比较 ，st.top()是下标
+            if (heights[i] > heights[st.top()]) {
+                st.push(i);
+                //cout<<heights.size()<<endl;
+            } else if (heights[i] == heights[st.top()]) {
+                st.pop(); // 这个可以加，可以不加，效果一样，思路不同
+                st.push(i);
+            } else {
+                while (heights[i] < heights[st.top()]) { // 注意是while
+                    int mid = st.top();
+                    st.pop();
+                    int left = st.top();
+                    int right = i;
+                    int w = right - left - 1;
+                    int h = heights[mid];
+                    result = max(result, w * h);
+                }
+                st.push(i);
+            }
+        }
+        return result;
+    }
+};
+
+// @lc code=end
+
