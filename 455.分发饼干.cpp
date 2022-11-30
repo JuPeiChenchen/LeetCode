64afdd442ebaf6f6d47dee32d9a1c40720bebce0
JuPeichen <1911524845@qq.com>
2022-11-30 10:23:50 +0800

    first

---
+++ 455.分发饼干.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=455 lang=cpp
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int result = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = g.size() - 1;
+        int j = s.size() - 1;
+        while(i >= 0 && j >= 0) {
+            if(g[i] <= s[j]) {
+                result++;
+                i--;
+                j--;
+            }
+            else {
+                i--;
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
+
