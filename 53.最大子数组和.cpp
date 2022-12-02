10f17c8b38b3a546cc82ad4e0e2ac57967ccab43
JuPeichen <1911524845@qq.com>
2022-12-02 22:19:03 +0800

    first

--- 53.最大子数组和.cpp
+++ 53.最大子数组和.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+/*class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         vector<int> dp(nums.size(), INT_MIN);
@@ -21,14 +21,19 @@ public:
         }
         return result;
     }
+};*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT32_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            count += nums[i];
+            if (count > result) { // 取区间累计的最大值（相当于不断确定最大子序终止位置）
+                result = count;
+            }
+            if (count <= 0) count = 0; // 相当于重置最大子序起始位置，因为遇到负数一定是拉低总和
+        }
+        return result;
+    }
 };
-// @lc code=end
--2
-1
--2
-4
-3
-5
-6
-1
-5
