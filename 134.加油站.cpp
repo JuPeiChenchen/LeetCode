ec506edaa7f9575ec8cee1b3594682a610e68ea0
JuPeichen <1911524845@qq.com>
2022-12-08 09:30:27 +0800

    first

---
+++ 134.加油站.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=134 lang=cpp
+ *
+ * [134] 加油站
+ */
+
+// @lc code=start
+/*class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        for (int i = 0; i < cost.size(); i++) {
+            int rest = gas[i] - cost[i]; // 记录剩余油量
+            int index = (i + 1) % cost.size();
+            while (rest > 0 && index != i) { // 模拟以i为起点行驶一圈
+                rest += gas[index] - cost[index];
+                index = (index + 1) % cost.size();
+            }
+            // 如果以i为起点跑一圈，剩余油量>=0，返回该起始位置
+            if (rest >= 0 && index == i) return i;
+        }
+        return -1;
+    }
+};*/
+/*class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int curSum = 0;
+        int min = INT_MAX; // 从起点出发，油箱里的油量最小值
+        for (int i = 0; i < gas.size(); i++) {
+            int rest = gas[i] - cost[i];
+            curSum += rest;
+            if (curSum < min) {
+                min = curSum;
+            }
+        }
+        if (curSum < 0) return -1;  // 情况1
+        if (min >= 0) return 0;     // 情况2
+                                    // 情况3
+        for (int i = gas.size() - 1; i >= 0; i--) {
+            int rest = gas[i] - cost[i];
+            min += rest;
+            if (min >= 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};*/
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {   // 当前累加rest[i]和 curSum一旦小于0
+                start = i + 1;  // 起始位置更新为i+1
+                curSum = 0;     // curSum从0开始
+            }
+        }
+        if (totalSum < 0) return -1; // 说明怎么走都不可能跑一圈了
+        return start;
+    }
+};
+// @lc code=end
+
