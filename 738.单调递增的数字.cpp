352437c6c970ff7bb83a8863d6bc356c681e0147
JuPeichen <1911524845@qq.com>
2022-12-16 11:58:41 +0800

    first

---
+++ 738.单调递增的数字.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=738 lang=cpp
+ *
+ * [738] 单调递增的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int monotoneIncreasingDigits(int N) {
+        string strNum = to_string(N);
+        // flag用来标记赋值9从哪里开始
+        // 设置为这个默认值，为了防止第二个for循环在flag没有被赋值的情况下执行
+        int flag = strNum.size();
+        for (int i = strNum.size() - 1; i > 0; i--) {
+            if (strNum[i - 1] > strNum[i] ) {
+                flag = i;
+                strNum[i - 1]--;
+            }
+        }
+        for (int i = flag; i < strNum.size(); i++) {
+            strNum[i] = '9';
+        }
+        return stoi(strNum);
+    }
+};
+// @lc code=end
+
