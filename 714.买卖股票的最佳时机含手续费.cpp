8626e034301055e0fc0196d63a09a35049375406
JuPeichen <1911524845@qq.com>
2022-12-17 09:57:02 +0800

    first

--- 714.买卖股票的最佳时机含手续费.cpp
+++ 714.买卖股票的最佳时机含手续费.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+/*class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
@@ -15,6 +15,29 @@ public:
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
         }
         return dp[prices.size() - 1][1];
+    }
+};*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int result = 0;
+        int minPrice = prices[0]; // 记录最低价格
+        for (int i = 1; i < prices.size(); i++) {
+            // 情况二：相当于买入
+            if (prices[i] < minPrice) minPrice = prices[i];
+
+            // 情况三：保持原有状态（因为此时买则不便宜，卖则亏本）
+            if (prices[i] >= minPrice && prices[i] <= minPrice + fee) {
+                continue;
+            }
+
+            // 计算利润，可能有多次计算利润，最后一次计算利润才是真正意义的卖出
+            if (prices[i] > minPrice + fee) {
+                result += prices[i] - minPrice - fee;
+                minPrice = prices[i] - fee; // 情况一，这一步很关键
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
