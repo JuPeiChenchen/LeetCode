460eb7612b1b93661e2cd70638b9b0b040f7ffce
JuPeichen <1911524845@qq.com>
2023-01-12 09:37:13 +0800

    first

---
+++ 52.n皇后-ii.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=cpp
+ *
+ * [52] N皇后 II
+ */
+
+// @lc code=start
+class Solution {
+private:
+int count = 0;
+void backtracking(int n, int row, vector<string>& chessboard) {
+    if (row == n) {
+        count++;
+        return;
+    }
+    for (int col = 0; col < n; col++) {
+        if (isValid(row, col, chessboard, n)) {
+            chessboard[row][col] = 'Q'; // 放置皇后
+            backtracking(n, row + 1, chessboard);
+            chessboard[row][col] = '.'; // 回溯
+        }
+    }
+}
+bool isValid(int row, int col, vector<string>& chessboard, int n) {
+    int count = 0;
+    // 检查列
+    for (int i = 0; i < row; i++) { // 这是一个剪枝
+        if (chessboard[i][col] == 'Q') {
+            return false;
+        }
+    }
+    // 检查 45度角是否有皇后
+    for (int i = row - 1, j = col - 1; i >=0 && j >= 0; i--, j--) {
+        if (chessboard[i][j] == 'Q') {
+            return false;
+        }
+    }
+    // 检查 135度角是否有皇后
+    for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+        if (chessboard[i][j] == 'Q') {
+            return false;
+        }
+    }
+    return true;
+}
+
+public:
+    int totalNQueens(int n) {
+        std::vector<std::string> chessboard(n, std::string(n, '.'));
+        backtracking(n, 0, chessboard);
+        return count;
+
+    }
+};
+// @lc code=end
+
