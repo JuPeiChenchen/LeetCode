987fa9fe497254f2bd5bb97efb6d11a5c8e5f9e7
JuPeichen <1911524845@qq.com>
2023-01-28 12:21:04 +0800

    first

---
+++ 1817.查找用户活跃分钟数.cpp
@@ -0,0 +1,17 @@
+ d
+ 
+ /*
+ * @lc app=leetcode.cn id=1817 lang=cpp
+ *
+ * [1817] 查找用户活跃分钟数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+
+    }
+};
+// @lc code=end
+
