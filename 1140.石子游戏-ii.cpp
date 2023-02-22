8f133430e05215a31c158a29b589acf06a4b57c0
JuPeichen <1911524845@qq.com>
2023-02-22 19:37:22 +0800

    first

---
+++ 1140.石子游戏-ii.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1140 lang=cpp
+ *
+ * [1140] 石子游戏 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int M = 1;
+        int result = 0;
+        
+    }
+};
+// @lc code=end
+
